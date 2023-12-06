53a1ccbbe859b9a31698a60fd02caaf39bae19b5
ackmanx <ackmanx@users.noreply.github.com>
2023-12-06 17:39:36 -0600

    Yay

--- src/data/local-storage.ts
+++ src/data/local-storage.ts
@@ -52,5 +52,6 @@ const exampleLocalStorageTabsData: Tab[] = [
   },
 ]
 
+// Local storage key: appContent
 const aboveExampleAsJSON =
   '[{"title":"Pull Requests","links":[{"label":"All of Mine","url":"http://git.target.com"},{"label":"FE - Ready for Review","url":"http://git.target.com"}],"cards":[{"title":"Frontend","links":[{"label":"All of Mine","url":"http://git.target.com"},{"label":"FE - Ready for Review","url":"http://git.target.com"}]},{"title":"Backend","links":[{"label":"All of Mine","url":"http://git.target.com"},{"label":"BE - Ready for Review","url":"http://git.target.com"}]}]},{"title":"Playgrounds","links":[],"cards":[{"title":"Frontend","links":[{"label":"All of Mine","url":"http://git.target.com"},{"label":"FE - Ready for Review","url":"http://git.target.com"}]}]},{"title":"Jira","links":[{"label":"Slingshot Jira Board","url":"http://jira.target.com"},{"label":"Slingshot Backlog","url":"http://jira.target.com"}],"cards":[]}]'
