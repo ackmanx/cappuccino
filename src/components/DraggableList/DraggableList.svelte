5700f214eb809b3ddae61919bda9024dee1503bd
Eric Majerus <Eric.Majerus@target.com>
2024-03-20 14:03:10 -0500

    Move

--- src/components/DraggableList/DraggableList.svelte
+++ src/components/DraggableList/DraggableList.svelte
@@ -4,15 +4,11 @@
     getLayerConfig,
     setContextByDraggableList,
   } from '../../context'
-  import Button from '../inputs/Button/Button.svelte'
 
   export let key: string
-  export let onSave: undefined | (() => void) = undefined
-  export let onNew: undefined | (() => void) = undefined
 
   setContextByDraggableList(key)
   const draggableList = getContextByDraggableList(key)
-  const layerConfig = getLayerConfig()
 
   const handleMouseOver = () => {
     $draggableList = { ...$draggableList, activeListArea: true }
@@ -20,18 +16,6 @@
 
   const handleMouseOut = () => {
     $draggableList = { ...$draggableList, activeListArea: false }
-  }
-
-  function handleCancel() {
-    $layerConfig = { ...$layerConfig, activate: false }
-  }
-
-  function handleSave() {
-    if (onSave) {
-      onSave()
-    }
-
-    handleCancel()
   }
 </script>
 
@@ -50,19 +34,6 @@
   >
     <slot />
   </ul>
-
-  {#if onNew}
-    <div>
-      <Button onClick={onNew}>new</Button>
-    </div>
-  {/if}
-
-  {#if onSave}
-    <div class="button-container">
-      <Button onClick={handleSave}>save</Button>
-      <Button variant="secondary" onClick={handleCancel}>cancel</Button>
-    </div>
-  {/if}
 </component>
 
 <!--
@@ -73,14 +44,6 @@
 <style>
   component {
     padding-bottom: 2rem;
-  }
-
-  .button-container {
-    display: flex;
-    justify-content: flex-end;
-    font-size: 1.6rem;
-    gap: 1rem;
-    padding: 1rem 0 0;
   }
 
   ul {
--- src/components/Drawer/drawers/ManageTabsDrawer.svelte
+++ src/components/Drawer/drawers/ManageTabsDrawer.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import type { Writable } from 'svelte/store'
 
+  import { getLayerConfig } from '../../../context'
   import type { ChangeEvent, TabType } from '../../../types'
   import DraggableItem from '../../DraggableList/DraggableItem.svelte'
   import DraggableList from '../../DraggableList/DraggableList.svelte'
+  import Button from '../../inputs/Button/Button.svelte'
   import TabInput from '../TabInput.svelte'
 
   export let tabs: Writable<TabType[]>
   export let isDragEnabled = false
   let tempTabs = structuredClone($tabs)
+  const layerConfig = getLayerConfig()
 
   function handleArrayUpdate(tabs: TabType[]) {
     tempTabs = tabs
@@ -21,6 +24,7 @@
   function handleTabSave() {
     $tabs = tempTabs
     localStorage.setItem('appContent', JSON.stringify($tabs))
+    handleCancel()
   }
 
   function handleUpdate(event: ChangeEvent, index: number) {
@@ -30,6 +34,10 @@
   function handleNewTab() {
     tempTabs = [...tempTabs, { title: '', cards: [], links: [] }]
   }
+
+  function handleCancel() {
+    $layerConfig = { ...$layerConfig, activate: false }
+  }
 </script>
 
 <!--
@@ -37,16 +45,42 @@
  │ ├┤ │││├─┘│  ├─┤ │ ├┤
  ┴ └─┘┴ ┴┴  ┴─┘┴ ┴ ┴ └─┘
 -->
-<DraggableList onSave={handleTabSave} onNew={handleNewTab} key="tabLinks">
-  {#each tempTabs as tab, index}
-    <DraggableItem
-      key="tabLinks"
-      onArrayUpdate={handleArrayUpdate}
-      {isDragEnabled}
-      dataArray={$tabs}
-      {index}
-    >
-      <TabInput onUpdateTab={handleUpdate} element={tab} {index} />
-    </DraggableItem>
-  {/each}
-</DraggableList>
+<component>
+  <DraggableList key="tabLinks">
+    {#each tempTabs as tab, index}
+      <DraggableItem
+        key="tabLinks"
+        onArrayUpdate={handleArrayUpdate}
+        {isDragEnabled}
+        dataArray={$tabs}
+        {index}
+      >
+        <TabInput onUpdateTab={handleUpdate} element={tab} {index} />
+      </DraggableItem>
+    {/each}
+  </DraggableList>
+
+  <div>
+    <Button onClick={handleNewTab}>new</Button>
+  </div>
+
+  <div class="button-container">
+    <Button onClick={handleTabSave}>save</Button>
+    <Button variant="secondary" onClick={handleCancel}>cancel</Button>
+  </div>
+</component>
+
+<!--
+┌─┐┌─┐┌─┐
+│  └─┐└─┐
+└─┘└─┘└─┘
+-->
+<style>
+  .button-container {
+    display: flex;
+    justify-content: flex-end;
+    font-size: 1.6rem;
+    gap: 1rem;
+    padding: 1rem 0 0;
+  }
+</style>
